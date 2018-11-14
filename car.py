1f5756f979bd62141d807feb1438cbfc1a607156
Ebloin <eliboninsegna@gmail.com>
2018-11-14 15:26:34 +0100

    add evaluate position in car.py

--- car.py
+++ car.py
@@ -30,7 +30,11 @@ class Car:
 		if msg.hop == msg.ttl:
 			return
 		
-		for c, i in zip(adj[plate], range(len(adj[plate])):
+		if (! evaluate_positions(self.messages, self.pos)):
+			return
+		#manca il controllo delle direzioni
+		
+		for c, i in zip(adj[plate], range(len(adj[plate]))):
 			if c == 1:
 				#Ho preso la macchina corrispondente
 				obj = Simulator.getCar(i)
@@ -45,6 +49,34 @@ class Car:
 			messages.append(msg)
 			thread.start_new_thread(timer, msg)
 		else messages.append(msg)
+	
+	def evaluate_positions(messages, my_pos):   # 1 messaggio solo  ## valuta se mandare in broadcast o no
+		quads = [0,0,0,0]			# flags dei quadranti: 0 quadrante inesplorato, 1 quadrante esplorato
+									# abbiamo scelto quadranti divisi da una X dalla nostra posizione
+		for m in messages:
+			if quads[0]!=1:
+				if m.emit[0]>my_pos[0] and  my_pos[1]-(abs((my_pos[0]-m.emit[0])/2)) <= m.emit[1] <= my_pos[1]-(abs((my_pos[0]-m.emit[0])/2)):		# X > myX	dx
+					quads[0]=1
+
+			if quads[1]!=1:
+				if m.emit[1]>my_pos[1] and (my_pos[0]-(abs((my_pos[1]-m.emit[1])/2)) <= m.emit[0] <= my_pos[0]-(abs((my_pos[1]-m.emit[1])/2))):		# Y > myY	su
+					quads[1]=1
+
+			if quads[2]!=1:
+				if m.emit[0]<my_pos[0] and (my_pos[1]-(abs((my_pos[0]-m.emit[0])/2)) <= m.emit[1] <= my_pos[1]-(abs((my_pos[0]-m.emit[0])/2))):		# X < myX	sx
+					quads[2]=1
+
+			if quads[3]!=1:
+				if m.emit[1]<my_pos[1] and (my_pos[0]-(abs((my_pos[1]-m.emit[1])/2)) <= m.emit[0] <= my_pos[0]-(abs((my_pos[1]-m.emit[1])/2))):		# Y < myY	giu
+					quads[3]=1
+					
+		print("Decidendo...")
+
+		if quads==[1,1,1,1]:
+			return False
+		else:
+			return True
+
 	
 	def timer(msg):
 		time.sleep(2) #aspetto 3 secondi che mi arrivino altri messaggi e si riempia il buffer messages
