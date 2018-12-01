b51aaac580ef1e18f36c8cba7a4e5c5d5407c4be
PierfrancescoConti <pconti97@libero.it>
2018-12-01 15:48:38 +0100

    stupid broad_msg

--- vis_test.py
+++ vis_test.py
@@ -1,22 +1,22 @@
-from visual import *
-#import alg
+from vpython import *
 import random
 import math
 import time
-import thread
-
+import threading
 
 
 #Calcolo la distanza, se e minore di R disegno freccia e rimando broadcast
 #p e la posizione attuale
 def broad_msg(balls, r, p):
 	for b in balls:
-		if b.color!=color.red:
-			if dist(p, b.pos) < r:
+	
+		if dist(p, b.pos) < r:
+			if b.color.value!=color.red.value:
 				b.color= color.red
-				arrow(pos=p, axis=(b.x - p.x, b.y - p.y, 0), shaftwidth=0.1, color=color.green)
+				arrow(pos=p, axis=vector(b.pos.x - p.x, b.pos.y - p.y, 0), shaftwidth=0.1, color=color.green)
 				time.sleep(0.5)
-				thread.start_new_thread(broad_msg, (balls, r, b.pos))
+				t = threading.Thread(target=broad_msg, args=(balls, r, b.pos))
+				t.start()
 
 
 def dist(v1, v2):
@@ -29,8 +29,7 @@ scene.x=0
 scene.y=0
 scene.width= 900
 scene.height=1000
-scene.center=(5,0,0)
-scene.background=(0.8, 0.8, 0.8)
+scene.center=vector(5,0,0)
 
 
 #Creo gli edifici (cubi)
@@ -58,19 +57,18 @@ for i in range(-40,50,20):
 #al click evidenzio il veicolo infettato
 flag = 0
 while True:
-        rate(30)
-        if scene.mouse.clicked:
-                m= scene.mouse.getclick()
-                loc = m.pos
-                loc.z = 0
-                print(loc)
-                for b in balls:
-                        if b.pos.x<loc.x+1 and b.pos.y<loc.y+1 and b.pos.x>loc.x-1 and b.pos.y>loc.y-1:
-                                b.color = color.red
-                                flag = 1
-                                break
-		if flag:
-			break
+    rate(30)
+    m=scene.waitfor('click')
+    loc = m.pos
+    loc.z = 0
+    print(loc)
+    for b in balls:
+        if b.pos.x<loc.x+1 and b.pos.y<loc.y+1 and b.pos.x>loc.x-1 and b.pos.y>loc.y-1:
+            b.color = color.red
+            flag = 1
+            break
+    if flag:
+        break
 
 
 #Parte l'algoritmo di infezione
