f167b2df4472f70243f8b69ebac3f9fd98f63676
Pietro Spadaccino <pietro.spa@gmail.com>
2019-03-08 11:52:38 +0100

    graphs style

--- grafici/graph-TC-comparison.py
+++ grafici/graph-TC-comparison.py
@@ -105,7 +105,7 @@ plt.ylabel('Nodes (%)')
 plt.title('')
 plt.xticks(ind+width+gap, ('Luxemburg', 'Cologne', 'New York'))
 plt.yticks(np.arange(0, 1.1, 0.1))
-plt.legend((p3[0], p2[0], p6[0], p4[0]), ('Forwarders', 'Receivers', r'Probabilistic $P=\widehat{P}$', r'Probabilistic $P=0.96$'), 
+plt.legend((p3[0], p2[0], p6[0], p4[0]), ('Relayers', 'Receivers', r'Probabilistic $P=\widehat{P}$', r'Probabilistic $P=0.96$'), 
 	loc='upper center', bbox_to_anchor=(0.5, -0.08),
     fancybox=True, shadow=True, ncol=5)
 
