c50e7f225ccb42b39f43f747e1e8a7b205321b3e
Pietro Spadaccino <pietro.spa@gmail.com>
2019-05-13 17:13:03 +0200

    improve network traffic graph

--- grafici/graph-TC-traffic-comparison.py
+++ grafici/graph-TC-traffic-comparison.py
@@ -17,7 +17,7 @@ def kilobytes(x, pos):
     '''The two args are the value and tick position'''
     return '%1.0fKB' % (x*1e-3)
 def bytes(x, pos):
-	return '%1.0fB' % x
+	return '%1.0f' % x
 
 formatter = FuncFormatter(kilobytes)
 formatter_bytes = FuncFormatter(bytes)
@@ -29,17 +29,17 @@ ax2.xaxis.set_major_formatter(formatter_bytes)
 
 
 
-#(249/790)*(5+22*10+x) > (608/790)*x
+#(249/790)*(7+22*6+x) > (608/790)*(7+x)
 
 x_lux = np.arange(0, 405, 1)
-l1 = (249) * (5+22*10+x_lux)
-l2 = (608) * x_lux
+l1 = (249) * (7+22*5+x_lux)
+l2 = (608) * (7+x_lux)
 p1 = ax1.plot(l1, label='EPIC Luxemburg')
 p2 = ax1.plot(l2, label='Probabilistic Luxembourg')
 
 x_col = np.arange(0, 405, 1)
-l1_col = 173 * (5+22*10+x_col)
-l2_col = 342 * x_col
+l1_col = 173 * (7+22*5+x_col)
+l2_col = 342 * (7+x_col)
 p3 = ax2.plot(l1_col, label='EPIC Cologne')
 p4 = ax2.plot(l2_col, label='Probabilistic Cologne')
 
@@ -58,8 +58,8 @@ ax2.set_xlim(0, 400)
 
 plt.gcf().subplots_adjust(left=0.15, right=0.95)
 
-#plt.show()
-plt.savefig('grafici/top_car/traffic_comparison.png', dpi=300)
+plt.show()
+#plt.savefig('grafici/top_car/traffic_comparison.png', dpi=300)
 
 
 
