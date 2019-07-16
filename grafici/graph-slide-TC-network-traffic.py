aeea94af87472827a25c4c8e6bb87875525bd570
Spadala <pietro.spa@gmail.com>
2019-07-16 08:41:10 +0200

    Added network traffic graph

--- grafici/graph-slide-TC-network-traffic.py
+++ grafici/graph-slide-TC-network-traffic.py
@@ -1,6 +1,7 @@
 
 from matplotlib import pyplot as plt 
 from matplotlib.ticker import FuncFormatter
+from random import random
 import numpy as np
 
 
@@ -30,22 +31,27 @@ ax.xaxis.set_major_formatter(formatter_bytes)
 
 #(249/790)*(7+22*6+x) > (608/790)*(7+x)
 
+b = 0.03
 overhead_msg  = 5+6
-overhead_epic = 22*3+overhead_msg
+overhead_epic = 14*4+overhead_msg
 
 f_epic, f_prb60, f_prb93, f_wp = 243, 478, 724, 569
 
 X_MAX = 175
-
-x_lux = np.arange(0, X_MAX+1, 5 )
-l1 = f_epic  * (overhead_epic+x_lux)
-l2 = f_prb93 * (overhead_msg+x_lux)
-l3 = f_prb60 * (overhead_msg+x_lux)
-l4 = f_wp    * (overhead_msg+x_lux)
-p1 = ax.plot(x_lux, l1, 'firebrick', linewidth=2)
-p2 = ax.plot(x_lux, l2, 'k', linewidth=2)
-p3 = ax.plot(x_lux, l3, 'b', linewidth=2)
-p4 = ax.plot(x_lux, l4, 'darkgreen', linewidth=2)
+arange_stop, arange_step = (X_MAX+1)/1, 5.0
+payload_len = np.arange(0, arange_stop, arange_step )
+l1 = f_epic  * (overhead_epic+payload_len)
+l2 = f_prb93 * (overhead_msg+payload_len)
+l3 = f_prb60 * (overhead_msg+payload_len)
+l4 = f_wp    * (overhead_msg+payload_len)
+#l1 += (2*np.random.random_sample(len(l1))-1)*b*l1
+#l2 += (2*np.random.random_sample(len(l2))-1)*b*l2
+#l3 += (2*np.random.random_sample(len(l3))-1)*b*l3
+#l4 += (2*np.random.random_sample(len(l4))-1)*b*l4
+p1 = ax.plot(payload_len, l1, 'firebrick', linewidth=2)
+p2 = ax.plot(payload_len, l2, 'k', linewidth=2)
+p3 = ax.plot(payload_len, l3, 'b', linewidth=2)
+p4 = ax.plot(payload_len, l4, 'darkgreen', linewidth=2)
 
 ax.legend((p1[0], p2[0], p3[0], p4[0]), ('EPIC', r'$P = 0.93$', r'$P = 0.6$', 'w-p-Persistence'))
 
