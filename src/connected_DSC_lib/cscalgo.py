6be1517864687d721ee9855c0c5718ad4a3d3b79
Andrea Sorrentino <andrea.sorrentino01@gmail.com>
2019-12-06 17:41:35 +0100

    update super Hero Vanet, Connected Set Cover, miss find shortest path

--- src/connected_DSC_lib/cscalgo.py
+++ src/connected_DSC_lib/cscalgo.py
@@ -13,40 +13,106 @@ class CscAlgo:
 		self.doAlgorithm()
 
 
-	def graphAdjacent(self, Sx, R):
-		pass
+
+	def get_neighbors(self, S):
+		"""
+		Returns (a deep copy of) the list of neighbors of vertex S in G
+		"""
+		return list(self.G[S][1:])
+
+
+	def find_shortest_path(self, R, T):
+		"""
+		Finds the shortest R->T path, from any node in R to T.
+		Returns a list of nodes P, where:
+			. P[0] is a node in R
+			. P[-1] is T
+			. Every other node in not in R
+		"""
+
+		# Take neighbors of T
+		T_neighbors = self.get_neighbors(T)
+
+		to_explore = list(T_neighbors)	#nodes to explore via the BFS
+		explored = set([T])				#nodes already explored via the BFS
+		next_nodes = dict({n: T for n in T_neighbors})	#maps a node to its successor in the R->T path
+		starting_node = None 			#first node of the R->T path
+		
+		# Do the BFS on the graph in "reverse": starting from node T and breaking when arriving to the first node in R
+		while len(to_explore) > 0:
+			node, to_explore = to_explore[0], to_explore[1:]
+
+			if node in R:
+				# This is the nearest node in R. I set it as the first node of the path and exit the BFS
+				starting_node = node
+				break
+			
+			for neighbor in self.get_neighbors(node):
+				# Append neighbors of `node` to the list of nodes to be explored
+				if not neighbor in explored:
+					next_nodes[neighbor] = node
+					to_explore.append(neighbor)
+
+			explored.add(node)
+
+		else:
+			raise Exception("starting_node not found")
+
+		# Use next_nodes to contruct path P
+		P = []	#path to be returned
+		node = starting_node
+		while node != T:
+			P.append(node)
+			node = next_nodes[node]
+		P.append(T)  #append last node T to path
+
+		return P
+
+
+	def graphAdjacent(self, S1, S2, G):
+		for elems1 in S1:
+
+			for setsS2 in S2:
+				for elsubsS2 in setsS2:
+
+					if elems1 in G[elsubsS2-1]:
+						return True
+
+		return False
+
+
 
 #remodule coverAdjacent with S1 and S2  
-	def coverAdjacent(self, Sx,R):
-		setSx	=	set(Sx)
-		for lsR	in	R:
-			setR	=	set(lsR)
+	def coverAdjacent(self, S1, S2):
+		setS1	=	set(S1)
+		for lS2	in	S2:
+			setS2	=	set(lS2)
 
-			if setSx.intersection(setR)	==	set():
+			if setS1.intersection(setS2)	==	set():
 				return	False
 		
 		return	True
 
 
-	def choseMaxS(self):
+	def choseMaxS(self,S):
 		#1 Chose S_0 € S_corsivo s.t. |S_0| is the maximum, and let R={S_0} and U = S_0
 		#return a list inside the first maximum |S_0|
-		return max(self.S, key=len)
+		return max(S, key=len)
 
-	def getElemSnotR(self):
+	def getElemS1notS2(self,S1,S2):
 		#where is a list of lists MORE ATTENTION HERE!
-		tempS	=	[]
+		tempS1	=	[]
 
-		for lsS in self.S:
-			setS	=	set(lsS)
-			for lsR in self.R:
-				setR	=	set(lsR)
+		for lsS1 in S1:
+			setS1	=	set(lsS1)
+			for lsS2 in S2:
+				setS2	=	set(lsS2)
 
-				# Sx \ R
-				if setS.difference(setR) != set():
-					tempS.append(list(setS))
+				# S1 \ S2
+				if setS1.difference(setS2) != set():
+					tempS1.append(list(setS1))
 					continue
-		return tempS
+		return tempS1
 
 	def doAlgorithm(self):
 		#######################################################################
@@ -56,28 +122,55 @@ class CscAlgo:
 		#######################################################################
 
 		################	FIRST STEP	###################################
-		S_0		=	self.choseMaxS()
+		#Choose S0 ∈ S such that |S0| is the maximum
+		S_0		=	self.choseMaxS(self.S)
 		
+		#let R = {S0} and U = S0
 		self.R.append(S_0)
 		self.U	=	S_0
-		################	END FIRST STEP	###############
+		
 
 		#################	SECOND STEP	################
-		#2 WHILE ELEMENTS OF V \ U NOT BELONG Ø DO...
+		# create a set of V and U
 
 		setV	=	set(self.V)
 		setU	=	set(self.U)
 
-
+		# While V \ U != ∅ DO
 		while setV.difference(setU) != set():
 
-			#start point 2.1
-			elemNotR	=	self.getElemSnotR()
+			# For each S ∈ S \ R which is cover-adjacent or graph-adjacent 
+			# with a set in R
+
+			elemNotR	=	self.getElemS1notS2(self.S,self.R)
 
 			for Sx	in	elemNotR:
-				print(Sx)
-				print(self.R)
-				print(self.coverAdjacent(Sx,self.R))
+				resultCovAdj	= self.coverAdjacent(Sx,self.R)
+				resultGraphAdj	= self.graphAdjacent(Sx,self.R,self.G)
+				
+				
+				if resultCovAdj or resultGraphAdj:
+					#Try It
+					shortest_Path = self.find_shortest_path(self.R,Sx)
+					
+					"""
+					lenghtPs = len(shortest_Path)
+
+					setShortPs = set(shortest_Path)
+
+					tempElemR = []
+					for sx in self.R:
+						for sxx in sx:
+							tempElemR.append(sxx)
+					
+					setElemR = set(tempElemR)
+
+					diff = setShortPs.difference(setElemR)
+
+					e_Ps = lenghtPs / len(diff)
+
+					"""
+				
 
 			break
 
