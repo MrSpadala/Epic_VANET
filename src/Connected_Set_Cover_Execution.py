a124063ff92fba517552b79f5ff3cda832f2bf37
Pietro Spadaccino <pietro.spa@gmail.com>
2020-02-15 15:50:51 +0100

    CSC algo now runs on 6 different scenarios

--- src/Connected_Set_Cover_Execution.py
+++ src/Connected_Set_Cover_Execution.py
@@ -3,120 +3,149 @@
 
 import time
 from simulator					import	Simulator, init_cars
+from sim_config					import 	config
 from connected_DSC_lib.parser	import	Parser
 from connected_DSC_lib.cscalgo	import	CscAlgo
 from connected_DSC_lib.gcreator import	Gcreator
 
-cars = init_cars()
+def execute_alg():
 
-sim = Simulator(cars)
+	cars = init_cars()
 
-parser	=	Parser(sim.car_dict)
+	sim = Simulator(cars)
 
-parser.CarToInputCSC()
+	parser	=	Parser(sim.car_dict)
 
-#Input initialization
-V	=	parser.V
-G	=	parser.G
-###################
+	parser.CarToInputCSC()
 
-
-
-#V=[1,2,3,4]
-#G=[[1,2],[2,1,3],[3,2],[4]]
-
-"""
-V=[0,1,2,3,4,5,6,7]
-G = [
-		[1,2,7],
-		[2,1,3,4,5,6],
-		[3,2,6],
-		[4,2,7],
-		[5,2],
-		[6,2,3],
-		[7,4,1]
-	]
-"""
-
-################### EXAMPLES
-
-"""
-GRAFO A MAGLIA
-V=[1,2,3,4,5,6,7]
-G = [
-		[1,2,7],
-		[2,1,3],
-		[3,2,4],
-		[4,3,5],
-		[5,4,6],
-		[6,5,7],
-		[7,6,1]
+	#Input initialization
+	V	=	parser.V
+	G	=	parser.G
+	###################
 
 
-]
-"""
 
-"""
-GRAFO A PERCORSO
-V=[1,2,3,4,5,6,7]
-G = [
-		[1,2],
-		[2,1,3],
-		[3,2,4],
-		[4,3,5],
-		[5,4.6],
-		[6,5,7],
-		[7,6]
+	#V=[1,2,3,4]
+	#G=[[1,2],[2,1,3],[3,2],[4]]
 
-]
-"""
-
-"""
-GRAFO AD ALBERO
-V=[1,2,3,4,5,6,7]
-G = [
-		[1,2,3],
-		[2,4,5],
-		[3,6,7],
-		[4,2],
-		[5,2],
-		[6,3],
-		[7,3]
-
-]
-
-V	=	[1,2,3,4,5,6,7,8,9,10]
-G	=	[
-			[1,2,3],
-			[2,1],
-			[3,1,4,7,10],
-			[4,3,5],
-			[5,4,8,6],
-			[6,5,7],
-			[7,6,3,9],
-			[8,5],
-			[9,7],
-			[10,3]
+	"""
+	V=[0,1,2,3,4,5,6,7]
+	G = [
+			[1,2,7],
+			[2,1,3,4,5,6],
+			[3,2,6],
+			[4,2,7],
+			[5,2],
+			[6,2,3],
+			[7,4,1]
 		]
-"""
-#initializate  CSC ALGO
-"""
-gCreator = Gcreator(500)
-gCreator.knitgraph()
-G = gCreator.G
-V = gCreator.V
-"""
-start_time = time.time()
-print('Start Algo for: %s nodes' % len(G))
+	"""
 
-cscAlgo	=	CscAlgo(V,G)
+	################### EXAMPLES
 
-connected_set_cover	=	cscAlgo.doConnectedSetCover()
-vertices_csc		=	list(map(lambda x: x[0], connected_set_cover))
-len_vertices_csc	=	len(vertices_csc)
+	"""
+	GRAFO A MAGLIA
+	V=[1,2,3,4,5,6,7]
+	G = [
+			[1,2,7],
+			[2,1,3],
+			[3,2,4],
+			[4,3,5],
+			[5,4,6],
+			[6,5,7],
+			[7,6,1]
 
-print(vertices_csc)
 
-print('End Algo TO CSC use only: %s nodes ' % len_vertices_csc)
+	]
+	"""
 
-print("--- %s seconds ---" % (time.time() - start_time))
+	"""
+	GRAFO A PERCORSO
+	V=[1,2,3,4,5,6,7]
+	G = [
+			[1,2],
+			[2,1,3],
+			[3,2,4],
+			[4,3,5],
+			[5,4.6],
+			[6,5,7],
+			[7,6]
+
+	]
+	"""
+
+	"""
+	GRAFO AD ALBERO
+	V=[1,2,3,4,5,6,7]
+	G = [
+			[1,2,3],
+			[2,4,5],
+			[3,6,7],
+			[4,2],
+			[5,2],
+			[6,3],
+			[7,3]
+
+	]
+
+	V	=	[1,2,3,4,5,6,7,8,9,10]
+	G	=	[
+				[1,2,3],
+				[2,1],
+				[3,1,4,7,10],
+				[4,3,5],
+				[5,4,8,6],
+				[6,5,7],
+				[7,6,3,9],
+				[8,5],
+				[9,7],
+				[10,3]
+			]
+	"""
+	#initializate  CSC ALGO
+	"""
+	gCreator = Gcreator(500)
+	gCreator.knitgraph()
+	G = gCreator.G
+	V = gCreator.V
+	"""
+	start_time = time.time()
+	print('Start Algo for: %s nodes' % len(G))
+
+	cscAlgo	=	CscAlgo(V,G)
+
+	connected_set_cover	=	cscAlgo.doConnectedSetCover()
+	vertices_csc		=	list(map(lambda x: x[0], connected_set_cover))
+	len_vertices_csc	=	len(vertices_csc)
+
+	print(vertices_csc)
+
+	print('End Algo TO CSC use only: %s nodes ' % len_vertices_csc)
+
+	print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+if __name__ == "__main__":
+	city_scenario = {
+		"Luxembourg": [
+			"time27100Tper1000.txt",
+			"time27100Tper50.txt"
+		],
+		"Cologne": [
+			"time23000Tper1000.txt",
+			"time23000Tper50.txt"
+		],
+		"NewYork": [
+			"Newyork7005.mat",
+			"Newyork3005.mat"
+		]
+	}
+
+	for city, scenarios in city_scenario.items():
+		for i, scenario in enumerate(scenarios):  #two scenarios per city, they must be in order high density then low density
+			config.city_name = city
+			config.scenario = scenario
+			
+			print(f"Executing algorithm on {city} {scenario}")
+			execute_alg()
