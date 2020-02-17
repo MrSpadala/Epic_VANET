9ea6278a8e01992bd7d632e595fb06aeca030518
Pietro Spadaccino <pietro.spa@gmail.com>
2020-02-17 17:10:58 +0100

    improve Tmax comparison

--- grafici/invited/Tmax-comparison.py
+++ grafici/invited/Tmax-comparison.py
@@ -5,136 +5,150 @@ import matplotlib.pyplot as plt
 
 import sys
 sys.path.append("src")
-from sim_config import config
+import sim_config
 from simulator import performSimulations, computeMetrics
 
-THETA = 0.0015  #mean of the underlying channel tx delay (s) 
-
-if config.city_name == "NewYork":
-    tmax_vals = np.linspace(0, 100, 11) * THETA
-elif config.city_name == "Luxembourg":
-    tmax_vals = np.linspace(0, 100, 11) * THETA
-elif config.city_name == "Cologne":
-    tmax_vals = np.linspace(0, 100, 11) * THETA
-else:
-    raise Exception(f"city {config.city_name} NA")
+THETA = 0.0025  #mean of the underlying channel tx delay (s) 
+tmax_vals = np.linspace(0, 40, 11) * THETA
 
 t_last_infctd_vals = np.zeros_like(tmax_vals)
 ratios_infected_vals = np.zeros_like(tmax_vals)
 sent_msgs_vals = np.zeros_like(tmax_vals)
 
 
-for i, tmax in enumerate(tmax_vals):
-    config.Tmax = tmax
-    
-    sims = performSimulations(config.nsimulations)
-    res = computeMetrics(sims)
-    n_cars, sent_msgs, recv_msgs, t_last_infect, cars_infected_ratio, network_traffic = res
+city_scenario = {
+    "Luxembourg": [
+        "time27100Tper1000.txt",
+        "time27100Tper50.txt"
+    ],
+    "Cologne": [
+        "time23000Tper1000.txt",
+        "time23000Tper50.txt"
+    ],
+    "NewYork": [
+        "Newyork7005.mat",
+        "Newyork3005.mat"
+    ]
+} 
 
-    t_last_infctd_vals[i] = t_last_infect
-    ratios_infected_vals[i] = cars_infected_ratio
-    sent_msgs_vals[i] = sent_msgs
+for city, scenarios in city_scenario.items():
+    for scenario in scenarios:
+        sim_config.config.city_name = city
+        sim_config.config.scenario = scenario
+        sim_config.load_opt_parameters()
+        print(f"Executing on {city} {scenario}")
 
-def mypprint(arr):
-    print("[", end="")
-    for v in arr:
-        print(f"{v}, ", end="")
-    print("]")
+        for i, tmax in enumerate(tmax_vals):
+            sim_config.config.Tmax = tmax
+            
+            sims = performSimulations(sim_config.config.nsimulations, verbose=False)
+            res = computeMetrics(sims, verbose=False)
+            n_cars, sent_msgs, recv_msgs, t_last_infect, cars_infected_ratio, network_traffic = res
 
-print("RATIOS")
-mypprint(ratios_infected_vals)
-print("SENT_MSGS")
-mypprint(sent_msgs_vals)
-print("TMAX VALS")
-mypprint(tmax_vals)
-print("T LAST INFCTD")
-mypprint(t_last_infctd_vals)
+            t_last_infctd_vals[i] = t_last_infect
+            ratios_infected_vals[i] = cars_infected_ratio
+            sent_msgs_vals[i] = sent_msgs
 
+        def mypprint(arr):
+            print("[", end="")
+            for v in arr:
+                print(f"{v}, ", end="")
+            print("]")
 
-def make_plot_frwd_recv(n_cars, means_recv_ratio, means_frwd, labels=None):
-    assert(len(means_recv_ratio) == len(means_frwd))
-    N = len(means_recv_ratio)
-
-    # pyplot config
-    plt.rcParams.update({'font.size': 18.3})
-    plt.rc('legend', fontsize=13)
-    plt.rc('xtick', labelsize=13.5)
-    plt.rc('ytick', labelsize=11.5)
-
-    means_frwd_ratio = np.asarray(means_frwd) / n_cars
-    means_recv_ratio = np.asarray(means_recv_ratio) - means_frwd_ratio
-
-
-    ind = np.arange(N)    # the x locations for the groups
-    width = 0.3       # the width of the bars: can also be len(x) sequence
-
-    #plt.bar(ind, means)
-
-    #fig, ax = plt.subplots()
-    fig = plt.figure()
-    ax = plt.subplot(111)
-
-    gap = 0.075
-
-    scale = 0.5
-    plt.figure(figsize=(15*scale, 10*scale))
-
-    p1 = plt.bar(ind, means_frwd_ratio, width, color='b', edgecolor='k')
-    p2 = plt.bar(ind, means_recv_ratio, width,
-                bottom=means_frwd_ratio, color='#ffa500', edgecolor='k')
+        print("RATIOS")
+        mypprint(ratios_infected_vals)
+        print("SENT_MSGS")
+        mypprint(sent_msgs_vals)
+        print("TMAX VALS")
+        mypprint(tmax_vals)
+        print("T LAST INFCTD")
+        mypprint(t_last_infctd_vals)
 
 
-    """
-    p3 = plt.bar(ind+width+gap, means_frw_low_dens, width, color='b', edgecolor='k')
-    p4 = plt.bar(ind+width+gap, means_recv_ratio_low_dens, width,
-                bottom=means_frw_low_dens, color='#cc6e00', edgecolor='k')
-    """
+        def make_plot_frwd_recv(n_cars, means_recv_ratio, means_frwd, labels=None):
+            assert(len(means_recv_ratio) == len(means_frwd))
+            N = len(means_recv_ratio)
+
+            # pyplot config
+            plt.rcParams.update({'font.size': 18.3})
+            plt.rc('legend', fontsize=13)
+            plt.rc('xtick', labelsize=13.5)
+            plt.rc('ytick', labelsize=11.5)
+
+            means_frwd_ratio = np.asarray(means_frwd) / n_cars
+            means_recv_ratio = np.asarray(means_recv_ratio) - means_frwd_ratio
 
 
-    #plt.text(2.4, 0.9,'drop_rate = 0.03',{'size':11})
+            ind = np.arange(N)    # the x locations for the groups
+            width = 0.3       # the width of the bars: can also be len(x) sequence
 
-    # Shrink current axis's height by 10% on the bottom
-    box = ax.get_position()
-    ax.set_position([box.x0, box.y0 + box.height * 0.1,
-                    box.width, box.height * 0.9])
+            #plt.bar(ind, means)
 
+            #fig, ax = plt.subplots()
+            fig = plt.figure()
+            ax = plt.subplot(111)
 
+            gap = 0.075
 
-    plt.ylabel('Nodes (%)')
-    plt.xlabel(r'$\frac{T_{max}}{\theta}$')
-    plt.title('')
-    if not labels is None:
-        plt.xticks(ind, labels) 
-    plt.yticks(np.arange(0, 1.1, 0.1))
-    plt.ylim((0.0, 1.0))
+            scale = 0.5
+            plt.figure(figsize=(15*scale, 10*scale))
 
-
-    #plt.legend((p1[0], p2[0], p4[0]), ('Relay', r'EPIC avg $\delta =43.8$', r'EPIC avg $\delta =11.6$'), 
-    #    loc='upper center', bbox_to_anchor=(0.465, 1.15), fancybox=True, shadow=True, ncol=5)
-
-    plt.legend((p1[0], p2[0]), ('Relay', 'Receivers'), 
-        loc='upper center', bbox_to_anchor=(0.465, 1.15), fancybox=True, shadow=True, ncol=5)
-
-    plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
-    #plt.show()
-    city, scenario = config.city_name, config.scenario
-    plt.savefig(f'grafici/invited/imgs/tmax/{city}-{scenario}.png', dpi=300)
+            p1 = plt.bar(ind, means_frwd_ratio, width, color='b', edgecolor='k')
+            p2 = plt.bar(ind, means_recv_ratio, width,
+                        bottom=means_frwd_ratio, color='#ffa500', edgecolor='k')
 
 
-def make_plot_time(tmax_vals, t_last_infctd_vals):
-    plt.plot(tmax_vals*1000, t_last_infctd_vals)
-    plt.ylabel('Transmission time (s)')
-    plt.xlabel(r'$T_{max}$ (ms)')
-    plt.show()
+            """
+            p3 = plt.bar(ind+width+gap, means_frw_low_dens, width, color='b', edgecolor='k')
+            p4 = plt.bar(ind+width+gap, means_recv_ratio_low_dens, width,
+                        bottom=means_frw_low_dens, color='#cc6e00', edgecolor='k')
+            """
 
 
-labels = map(lambda x: f"{x/THETA:.0f}", tmax_vals)
-make_plot_frwd_recv(
-    n_cars,
-    ratios_infected_vals,
-    sent_msgs_vals,
-    labels=labels
-)
+            #plt.text(2.4, 0.9,'drop_rate = 0.03',{'size':11})
+
+            # Shrink current axis's height by 10% on the bottom
+            box = ax.get_position()
+            ax.set_position([box.x0, box.y0 + box.height * 0.1,
+                            box.width, box.height * 0.9])
+
+
+
+            plt.ylabel('Nodes (%)')
+            plt.xlabel(r'$\frac{T_{max}}{\theta}$')
+            plt.title('')
+            if not labels is None:
+                plt.xticks(ind, labels) 
+            plt.yticks(np.arange(0, 1.1, 0.1))
+            plt.ylim((0.0, 1.0))
+
+
+            #plt.legend((p1[0], p2[0], p4[0]), ('Relay', r'EPIC avg $\delta =43.8$', r'EPIC avg $\delta =11.6$'), 
+            #    loc='upper center', bbox_to_anchor=(0.465, 1.15), fancybox=True, shadow=True, ncol=5)
+
+            plt.legend((p1[0], p2[0]), ('Relay', 'Receivers'), 
+                loc='upper center', bbox_to_anchor=(0.465, 1.15), fancybox=True, shadow=True, ncol=5)
+
+            plt.gcf().subplots_adjust(bottom=0.15, left=0.15)
+            #plt.show()
+            city, scenario = sim_config.config.city_name, sim_config.config.scenario
+            plt.savefig(f'grafici/invited/imgs/tmax/{city}-{scenario}.png', dpi=300)
+
+
+        def make_plot_time(tmax_vals, t_last_infctd_vals):
+            plt.plot(tmax_vals*1000, t_last_infctd_vals)
+            plt.ylabel('Transmission time (s)')
+            plt.xlabel(r'$T_{max}$ (ms)')
+            plt.show()
+
+
+        labels = map(lambda x: f"{x/THETA:.0f}", tmax_vals)
+        make_plot_frwd_recv(
+            n_cars,
+            ratios_infected_vals,
+            sent_msgs_vals,
+            labels=labels
+        )
 
 """
 make_plot_time(
