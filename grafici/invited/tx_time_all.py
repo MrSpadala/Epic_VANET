ebf8f7a08ad40a6b48d7ad6fff5b4870f7783240
Pietro Spadaccino <pietro.spa@gmail.com>
2020-02-18 17:43:03 +0100

    update tx graphs

--- grafici/invited/tx_time_all.py
+++ grafici/invited/tx_time_all.py
@@ -3,15 +3,15 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # Time of last vehicle infection for all the topologies
-newyork_7005 = [0.00755, 0.0437, 0.0801, 0.1158, 0.16335, 0.19315000000000002, 0.23915, 0.27245, 0.3128, 0.34935, 0.38005, ]
-newyork_3005 = [0.0076500000000000005, 0.0443, 0.07980000000000001, 0.12725, 0.1492, 0.18675, 0.217, 0.2606, 0.2869, 0.32545, 0.38235, ]
-luxembourg_1000 = [0.00935, 0.05185, 0.10325000000000001, 0.15445, 0.20850000000000002, 0.25075000000000003, 0.29975, 0.32305, 0.37255, 0.42925, 0.46795000000000003, ]
-luxembourg_50 = [0.01705, 0.12575, 0.2265, 0.3211, 0.44265000000000004, 0.5555, 0.6438, 0.7100500000000001, 0.8856, 0.9618000000000001, 1.0461, ]
-cologne_1000 = [0.01955, 0.12145, 0.22690000000000002, 0.33585000000000004, 0.44225000000000003, 0.51715, 0.6585000000000001, 0.74145, 0.8527, 0.95435, 1.0915000000000001, ]
-cologne_50 = [0.01755, 0.11495000000000001, 0.19790000000000002, 0.30625, 0.3947, 0.4923, 0.55805, 0.641, 0.72355, 0.8239000000000001, 0.9342, ]
+newyork_7005 = 1000 * np.array([0.007350000000000001, 0.019350000000000003, 0.03065, 0.043500000000000004, 0.05745, 0.06805, 0.0792, 0.09395, 0.11405, 0.1188, 0.13745000000000002, ])
+newyork_3005 = 1000 * np.array([0.0076500000000000005, 0.0198, 0.03355, 0.04505, 0.05885, 0.06935000000000001, 0.07965, 0.09620000000000001, 0.11025, 0.1105, 0.12645, ])
+luxembourg_1000 = 1000 * np.array([0.009550000000000001, 0.0239, 0.04125, 0.05885, 0.07295, 0.09095, 0.10745, 0.11415, 0.1306, 0.1484, 0.16315000000000002, ])
+luxembourg_50 = 1000 * np.array([0.0176, 0.056, 0.0917, 0.12090000000000001, 0.15625, 0.19125, 0.227, 0.26745, 0.3042, 0.3317, 0.38470000000000004, ])
+cologne_1000 = 1000 * np.array([0.0223, 0.04995, 0.0806, 0.12445, 0.15885000000000002, 0.19265000000000002, 0.24735000000000001, 0.25775000000000003, 0.28435, 0.33485000000000004, 0.34855, ])
+cologne_50 = 1000 * np.array([0.0181, 0.0482, 0.07995000000000001, 0.10990000000000001, 0.1427, 0.17395000000000002, 0.188, 0.2366, 0.2631, 0.30295, 0.29935, ])
 
 # tmax values values
-x = np.linspace(0, 100, 11)
+x = np.linspace(0, 20, 11)
 
 def plot_config():
     plt.rcParams.update({'font.size': 18.3})
@@ -21,9 +21,9 @@ def plot_config():
     plt.rc('grid', linestyle="--", color='black')
     plt.gcf().subplots_adjust(bottom=0.16)
 
-    plt.ylabel('Transmission time (s)')
-    plt.xlabel(r'$\frac{T_{max}}{\theta}$')
-    plt.ylim((-0.05, 1.15))
+    plt.ylabel('Transmission time (ms)')
+    plt.xlabel(r'$\frac{T_{max}}{T_{tx}}$')
+    plt.ylim((-50, 500))
 
     plt.grid(True)
 
@@ -36,7 +36,7 @@ plt.plot(x, newyork_7005, marker="o", color="blue", linewidth=0.8)
 plt.plot(x, luxembourg_1000, marker="s", color="red", linewidth=0.8)
 plt.plot(x, cologne_1000, marker="^", color="green", linewidth=0.8)
 plt.title("High Density", fontsize=15)
-plt.legend()
+plt.legend(loc="upper left")
 #plt.show()
 plt.savefig("grafici/invited/imgs/tx_time/all_high_dens.png", dpi=300)
 plt.clf()
@@ -49,6 +49,6 @@ plt.plot(x, newyork_3005, marker="o", color="blue", linewidth=0.8)
 plt.plot(x, luxembourg_50, marker="s", color="red", linewidth=0.8)
 plt.plot(x, cologne_50, marker="^", color="green", linewidth=0.8)
 plt.title("Low Density", fontsize=15)
-plt.legend()
+plt.legend(loc="upper left")
 #plt.show()
 plt.savefig("grafici/invited/imgs/tx_time/all_low_dens.png", dpi=300)
