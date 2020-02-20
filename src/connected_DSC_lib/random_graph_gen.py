e7f35364fd1aeae6fa2db9a3e305528700dc5bdc
Andrea Sorrentino <andrea.sorrentino01@gmail.com>
2020-02-20 18:00:29 +0100

    create a class for generation random graph

--- src/connected_DSC_lib/random_graph_gen.py
+++ src/connected_DSC_lib/random_graph_gen.py
@@ -1,13 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import networkx as nx
 
-
+class RandomGrapGen():
+	def __init__(self, nodes, probability):
+		
+		self.nodes			=	nodes
+		self.probability	=	probability
+		
 
 n	=	100		#nodes
 
 p	=	0.3		#probability edge creation
 
 
-grap = nx.fast_gnp_random_graph(n=n,p=p)
+	def ParseToCSCInput(self):
+		'''This function is used to parse
+			this random graph gen for input CSC'''
 
-print(nx.nodes(grap))
-print(nx.edges(grap))
+
+	grap = nx.fast_gnp_random_graph(n=n,p=p)
+
+	print(nx.nodes(grap))
+	print(nx.edges(grap))
